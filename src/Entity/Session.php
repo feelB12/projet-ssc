76e373c1fd347777389348a04f70f9d2dc4cfd35
feelB12git config --global user.mname feelB12 <philippebidouze@gmail.com>
2022-04-20 18:47:25 +0200

    add+entity+sessionnonull

--- src/Entity/Session.php
+++ src/Entity/Session.php
@@ -18,7 +18,7 @@ class Session
     private $id;
 
     /**
-     * @ORM\Column(type="string", length=255, nullable=true)
+     * @ORM\Column(type="string", length=255)
      */
     private $title;
 
