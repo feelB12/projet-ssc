ea7cd2c00d29e4f1c72a308eff29dcf4d6c11eeb
feelB12git config --global user.mname feelB12 <philippebidouze@gmail.com>
2022-04-21 20:33:45 +0200

    sessioncontroller+twig

---
+++ src/Controller/SessionController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Session;
+use App\Repository\SessionRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+class SessionController
+{
+    /**
+     * @Route("/sessions", name="sessions")
+     */
+    public function sessions(SessionRepository $sessionRepository)
+    {
+        $sessions = $sessionRepository->findAll();
+        return $this->render('sessions.html.twig', [
+            'sessions' => $sessions
+        ]);
+    }
+
+    /**
+     * @Route("/session/{id}", name="session")
+     */
+    public function session($id, SessionRepository $sessionRepository)
+    {
+        $session = $sessionRepository->find($id);
+        return $this->render('session.html.twig', [
+            'session' => $session
+        ]);
+    }
+}
